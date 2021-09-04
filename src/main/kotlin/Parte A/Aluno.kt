5e7d56c5dd62b88b809e2300bb1f227bceb5e035
itonAntunes <iton.antunes@gmail.com>
2021-09-03 23:15:26 -0300

    Criando a Classe Aluno e criando exemplos de Alunos

---
+++ src/main/kotlin/Parte A/Aluno.kt
@@ -0,0 +1,11 @@
+package `Parte A`
+// Criação da classe aluno
+class Aluno (nome:String, sobrenome:String, codigoDoAluno:Int) {
+
+    init {
+        var aluno1 = Aluno("Everton","Antunes",1)
+        var aluno2 = Aluno("Everton","Antunes",1)
+        var aluno3 = Aluno("Everton","Antunes",1)
+    }
+
+}
