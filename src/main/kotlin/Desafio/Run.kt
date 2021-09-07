16d97fad1f1532dd207b9d19cda59b533afd9b23
itonAntunes <iton.antunes@gmail.com>
2021-09-06 22:06:51 -0300

    Conclusão da Parte B do Desafio

--- src/main/kotlin/Desafio/Run.kt
+++ src/main/kotlin/Desafio/Run.kt
@@ -3,10 +3,10 @@ package Desafio
 fun main() {
     var aluno1 = Aluno("Everton", "Antunes", 1)
     var aluno2 = Aluno("Everton", "Antunes", 2)
-    var aluno3 = Aluno("Maria", "Oliveira", 2)
+    var aluno3 = Aluno("Maria", "Oliveira", 3)
     var aluno4 = Aluno("João", "Cardoso", 4)
 
-    var curso1 = Curso("Mecânica",3)
+    var curso1 = Curso("Mecânica",1)
     var curso2 = Curso("Elétrica",2)
     var curso3 = Curso("Hidráulica",3)
     var curso4 = Curso("Costura",4)
