752eb3686c9795f931e84569615fcbb72294b7c9
itonAntunes <iton.antunes@gmail.com>
2021-09-06 21:03:38 -0300

    Conclusão da Parte A do Desafio

--- src/main/kotlin/Parte A/Aluno.kt
+++ src/main/kotlin/Parte A/Aluno.kt
@@ -1,41 +1,19 @@
 package `Parte A`
+
+import javax.print.attribute.standard.MediaSize
+
 // Criação da classe aluno
-class Aluno (var nome:String, var sobrenome:String, var codigoDoAluno:Int) {
-    override fun equals(other: Any?): Boolean {
-        if (other === this){
-            return true
-
-        }
-        if (other !is Aluno){
-            return false
-
-        }
-        val comparador = other as Aluno?
-        return  codigoDoAluno == comparador?.codigoDoAluno
+class Aluno(var nome: String, var sobrenome: String, var codigoDoAluno: Int) {
+    companion object {
+        val codigoAlunoCriado = mutableListOf<Int>()
     }
-
-
-
     init {
-        println("Novo Aluno Cadastrado")
-        println(" ${nome} ${sobrenome} ${codigoDoAluno}" )
+        if (codigoAlunoCriado.contains(codigoDoAluno)) {
+            println("Aluno: $nome $sobrenome com ID ${codigoDoAluno} já cadastrado !!")
+        } else {
+            codigoAlunoCriado.add(codigoDoAluno)
+            println("Novo aluno $nome $sobrenome !!")
+        }
         println(" ")
-
-
     }
-
-}
-
-fun main() {
-    var aluno1 = Aluno("Everton", "Antunes", 1)
-    var aluno2 = Aluno("Everton","Antunes",1)
-    var aluno3 = Aluno("Maria","Oliveira",3)
-    var aluno4 = Aluno("João","Cardoso",4)
-
-    if (aluno1 == aluno2){
-        println("Temos alunos cadastrados com o mesmo Codigo Identificador")
-    } else {
-        println("não temos alunos Duplicados, pode prosseguir")
-    }
-
 }
---
+++ src/main/kotlin/Parte A/Run.kt
@@ -0,0 +1,8 @@
+package `Parte A`
+
+fun main() {
+    var aluno1 = Aluno("Everton", "Antunes", 1)
+    var aluno2 = Aluno("Everton", "Antunes", 2)
+    var aluno3 = Aluno("Maria", "Oliveira", 3)
+    var aluno4 = Aluno("João", "Cardoso", 4)
+}
